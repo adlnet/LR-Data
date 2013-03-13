81c3423867064cfe69665556a26769783f93af9f
Walt Grata <wegrata@gmail.com>
2013-03-13 10:01:54 -0400

    refactored to make better use of celery

--- src/tasks/save.py
+++ src/tasks/save.py
@@ -2,15 +2,15 @@ import couchdb
 from celery.task import task
 from celery.log import get_default_logger
 import redis
-from urlparse import urlparse
 import nltk
 import requests
 import hashlib
 from lxml import etree
 from StringIO import StringIO
 import subprocess
+from BeautifulSoup import BeautifulSoup
 import os
-from celery import group, chain
+from celery import group, chain, chord
 log = get_default_logger()
 dc_namespaces = {"nsdl_dc": "http://ns.nsdl.org/nsdl_dc_v1.02/",
                  "dc": "http://purl.org/dc/elements/1.1/",
@@ -19,31 +19,51 @@ dc_namespaces = {"nsdl_dc": "http://ns.nsdl.org/nsdl_dc_v1.02/",
 stop_words = requests.get("http://jmlr.csail.mit.edu/papers/volume5/lewis04a/a11-smart-stop-list/english.stop").text.split("\n")
 
 
+def create_nsdl_dc_task_tree(data, config):
+    parse_sub_task = parse_nsdl_dc.s(data, config)
+    common_core_chain = (parse_sub_task | handle_common_core.s() | process_keywords.s())
+    nsdl_chain = (parse_sub_task | nsdl_keyword.s() | process_keywords.s())
+    nsdl_display_chain = (parse_sub_task | nsdl_display_data.s())
+    return [common_core_chain, nsdl_chain, nsdl_display_chain]
+
+
 @task(queue="save")
 def createRedisIndex(data, config):
     key_tasks = []
+    display_found = False
     envelope_chain = (parse_envelope_keywords.s(data, config) | process_keywords.s())
     key_tasks.append(envelope_chain)
     schemas = [x.lower() for x in data['payload_schema']]
     if "nsdl_dc" in schemas:
-        common_core_chain = (handle_common_core.s() | process_keywords.s())
-        parse_result_group = group(nsdl_keyword.s(), common_core_chain)
-        parse_chain = (parse_nsdl_dc.s(data, config) | parse_result_group)
-        save_nsdl_keys = (parse_chain | process_keywords.s())
-        key_tasks.append(save_nsdl_keys)
-
+        display_found = True
+        key_tasks.extend(create_nsdl_dc_task_tree(data, config))
+    elif "lrmi" in schemas:
+        display_found = True
+        lrmi_chain = (parse_lrmi_keywords.s(data, config) | process_keywords.s())
+        key_tasks.append(lrmi_chain)
+        key_tasks.append(lrmi_display_data.s(data, config))
     html_chain = (parse_html.s(data, config) | process_keywords.s())
     key_tasks.append(html_chain)
+    if not display_found:
+        key_tasks.append(html_display_data.s(data, config))
+    key_tasks.append(save_image.s(data, config))
     group(key_tasks)()
 
 
 def save_to_index(k, value, r):
     keywords = k.split(' ')
     keywords.append(k)
+    punctuation = range(32, 48)
+    punctuation.extend(range(58, 65))
+    punctuation.extend(range(91, 97))
+    punctuation.extend(range(123, 128))
     for keyword_part in keywords:
         if keyword_part in stop_words:
-            continue
-        log.debug(keyword_part)
+            continue  # don't index stop words
+        if len(keyword_part) == 1:
+            continue  # don't index single characters
+        if reduce(lambda x, y: x and (ord(y) in punctuation), keyword_part, True):
+            continue  # don't index if the entire string is punctuation
         print(keyword_part)
         if not r.zadd(keyword_part, 1.0, value):
             r.zincrby(keyword_part, value, 1.0)
@@ -57,9 +77,7 @@ def parse_envelope_keywords(data, config):
 @task(queue="parse")
 def parse_nsdl_dc(data, config):
     try:
-        s = StringIO(data['resource_data'])
-        tree = etree.parse(s)
-        return data['resource_locator'], tree, config
+        return data['resource_locator'], data['resource_data'], config
     except etree.XMLSyntaxError:
         print(data['resource_data'])
 
@@ -79,7 +97,9 @@ def process_keywords(args):
 
 @task(queue="index")
 def nsdl_keyword(args):
-    resource_locator, tree, config = args
+    resource_locator, raw_tree, config = args
+    s = StringIO(raw_tree)
+    tree = etree.parse(s)
     try:
         result = tree.xpath('/nsdl_dc:nsdl_dc/dc:subject',
                             namespaces=dc_namespaces)
@@ -90,21 +110,26 @@ def nsdl_keyword(args):
 
 @task(queue="parse")
 def handle_common_core(args):
-    url, tree, config = args
+    url, raw_tree, config = args
+    s = StringIO(raw_tree)
+    tree = etree.parse(s)
     r = redis.StrictRedis(host=config['redis']['host'],
                           port=config['redis']['port'],
                           db=config['redis']['db'])
-    query = "/nsdl_dc:nsdldc/dct:conformsTo"
-    result = tree.query(query, namespaces=dc_namespaces)
+    query = "/nsdl_dc:nsdl_dc/dct:conformsTo"
+    result = tree.xpath(query, namespaces=dc_namespaces)
     keywords = []
     for standard in result:
-        r.incr(standard.text)
+        r.incr(standard.text+"-count")
         keywords.append(standard.text)
     return keywords, url, config
 
 
 @task(queue="display")
-def nsdl_display_data(resource_locator, tree, config):
+def nsdl_display_data(args):
+    resource_locator, raw_tree, config = args
+    s = StringIO(raw_tree)
+    tree = etree.parse(s)
     result = tree.xpath('/nsdl_dc:nsdl_dc/dc:title',
                         namespaces=dc_namespaces)
     title = result[0].text
@@ -114,7 +139,22 @@ def nsdl_display_data(resource_locator, tree, config):
     result = tree.xpath('/nsdl_dc:nsdl_dc/dc:publisher',
                         namespaces=dc_namespaces)
     publisher = result[0].text
+    print(title)
+    print(description)
     save_display_data(title, description, publisher, resource_locator, config)
+
+
+@task(queue="parse")
+def parse_lrmi_keywords(data, config):
+    metadata = data['resource_data']['items']
+    keywords = []
+    for item in metadata:
+        keywords.extend(item['mediaType'])
+        properties = item['properties']
+        keywords.extend(properties['about'])
+        keywords.extend(properties['name'])
+        keywords.extend(properties['description'])
+    return keywords, data['resource_locator'], config
 
 
 @task(queue="display")
@@ -130,50 +170,49 @@ def lrmi_display_data(data, config):
 def parse_html(data, config):
     url = data['resource_locator']
     resp = requests.get(url)
-    raw = nltk.clean_html(resp.text)
-    tokens = [t.lower for t in nltk.word_tokenize(raw)]
+    if "html" not in resp.headers['content-type']:
+        return [], url, config
+    tokens = []
+    if resp.text is not None:
+        raw = nltk.clean_html(resp.text)
+        tokens = [t.lower() for t in nltk.word_tokenize(raw)]
     return tokens, url, config
 
 
-
-'''
-def save_display_data(parts, data, config):
-    title = data['resource_locator']
-    description = ""
-    publisher = None
-
-    try:
-        headers = requests.head(data['resource_locator'])
-        if 'nsdl_dc' in data['payload_schema']:
-
-        elif "LRMI" in data['payload_schema']:
-
-        elif headers.headers['content-type'].startswith('text/html'):
-            fullPage = requests.get(data['resource_locator'])
-            soup = BeautifulSoup(fullPage.content)
-            title = soup.html.head.title.string
+def format_publisher(publisher, data):
+    if publisher is None:
+        curator = data['identity'].get("curator", None)
+        owner = data['identity'].get("owner", None)
+        if curator is not None and owner is not None and curator is not owner:
+            publisher = "{0}, supported by {1}".format(curator, owner)
+        elif curator is not None:
+            publisher = curator
         else:
-            title = "{0}/...{1}".format(parts.netloc,
-                                        parts.path[parts.path.rfind('/'):])
-        if publisher is None:
-            curator = data['identity'].get("curator", None)
-            owner = data['identity'].get("owner", None)
-            if curator is not None and owner is not None and curator != owner:
-                publisher = "{0}, supported by {1}".format(curator, owner)
-            elif curator is not None:
-                publisher = curator
+            signer = data['identity'].get("signer", "")
+            submitter = data['identity'].get("submitter", "")
+            if len(signer) > len(submitter):
+                publisher = signer
+            elif len(submitter) > len(signer):
+                publisher = submitter
             else:
-                signer = data['identity'].get("signer", "")
-                submitter = data['identity'].get("submitter", "")
-                if len(signer) > len(submitter):
-                    publisher = signer
-                elif len(submitter) > len(signer):
-                    publisher = submitter
-                else:
-                    publisher = ""
-    except Exception as e:
-        print(e)
-'''
+                publisher = ""
+    return publisher
+
+
+@task(queue="display")
+def html_display_data(data, config):
+    url = data['resource_locator']
+    resp = requests.get(url)
+    soup = BeautifulSoup(resp.content)
+    if soup.html is not None:
+        title = soup.html.head.title.string
+    else:
+        title = url
+    raw = nltk.clean_html(resp.text)
+    tokens = nltk.word_tokenize(raw)
+    description = " ".join(tokens[:100])
+    publisher = format_publisher(None, data)
+    save_display_data(title, description, publisher, url, config)
 
 
 def save_display_data(title, description, publisher, resource_locator, config):
@@ -186,13 +225,11 @@ def save_display_data(title, description, publisher, resource_locator, config):
         doc = {"_id": couchdb_id}
         if couchdb_id in db:
             doc = db[couchdb_id]
-        doc["title"] = title
-        doc["description"] = description
-        doc["url"] = resource_locator
-        doc['publisher'] = publisher
-        print(doc)
+        doc["title"] = title.strip()
+        doc["description"] = description.strip()
+        doc["url"] = resource_locator.strip()
+        doc['publisher'] = publisher.strip()
         db[couchdb_id] = doc
-        print(doc)
         print("saved to couch")
     except Exception as ex:
         print(ex)
@@ -203,23 +240,21 @@ def save_image(envelope, config):
     m = hashlib.md5()
     m.update(envelope['resource_locator'])
     couchdb_id = m.hexdigest()
-    print('get lock')
     p = subprocess.Popen(" ".join(["xvfb-run", "--auto-servernum", "--server-num=1", "python", "screenshots.py", envelope['resource_locator'], couchdb_id]), shell=True, cwd=os.getcwd(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     filename = p.communicate()
     print(filename)
-    print(couchdb_id)
     db = couchdb.Database(config['couchdb']['dbUrl'])
+    if couchdb_id not in db:
+        db[couchdb_id] = {"_id": couchdb_id}
     try:
         with open(os.path.join(os.getcwd(), couchdb_id+"-thumbnail.jpg"), "rb") as f:
             db.put_attachment(db[couchdb_id], f, "thumbnail.jpeg", "image/jpeg")
-            print("uploaded")
     except IOError as e:
         log.debug(os.path.join(os.getcwd(), couchdb_id+"-thumbnail.jpg"))
         log.exception(e)
     try:
         with open(os.path.join(os.getcwd(), couchdb_id+"-screenshot.jpg"), "rb") as f:
             db.put_attachment(db[couchdb_id], f, "screenshot.jpeg", "image/jpeg")
-            print("uploaded")
     except IOError as e:
         log.exception(e)
     for file_to_delete in [couchdb_id+"-thumbnail.jpg", couchdb_id+"-screenshot.jpg", couchdb_id + ".jpg"]:
@@ -227,4 +262,4 @@ def save_image(envelope, config):
             os.remove(os.path.join(os.getcwd(), file_to_delete))
         except OSError as e:
             log.error(os.path.join(os.getcwd(), file_to_delete))
-            print(e)
+    print(couchdb_id)
