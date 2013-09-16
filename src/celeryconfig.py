f61c4986374db96cb09136ed0b11a252e86a6e28
Walt Grata <wegrata@gmail.com>
2013-09-16 08:56:28 -0400

    update config, rabbitmq was giving memory issues

--- src/celeryconfig.py
+++ src/celeryconfig.py
@@ -25,12 +25,14 @@ CELERY_IMPORTS = ("tasks.harvest", "tasks.save", "tasks.validate", "tasks.displa
 CELERY_RESULT_BACKEND = 'amqp://'
 CELERY_TASK_RESULT_EXPIRES = 15
 ## Broker settings.
-BROKER_URL = 'amqp://'
+BROKER_URL = 'redis://localhost:6379/2'
 
 CELERY_LOG_DEBUG = "TRUE"
 CELERY_LOG_FILE = "./celeryd.log"
-CELERY_LOG_LEVEL = "INFO"
-BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 600}
+CELERY_LOG_LEVEL = "DEBUG"
+BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 3600}
+CELERY_RESULT_BACKEND = "redis://localhost/1"
+CELERY_TASK_RESULT_EXPIRES = 1
 ## Worker settings
 ## If you're doing mostly I/O you can have more processes,
 ## but if mostly spending CPU, try to keep it close to the
