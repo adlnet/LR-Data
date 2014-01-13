dc6f9cd9fa0456743173f18c7931bca422606128
Walt Grata <wegrata@gmail.com>
2014-01-13 12:04:43 -0500

    Update start.py

--- src/start.py
+++ src/start.py
@@ -2,5 +2,4 @@ from tasks.harvest import *
 from celeryconfig import config
 from redis import StrictRedis
 r = StrictRedis(db=0)
-r.set("lastHarvestTime", "2013-12-01T00:00:00Z")
 startHarvest(config)
